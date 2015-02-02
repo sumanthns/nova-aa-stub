eb33c3d03f94f5d937a5bdfc69779d4f562961ac
Sumanth <sumansn@thoughtworks.com>
2015-02-02 17:11:41 -0600

    Made route tenant_id agnostic

--- novastub/__init__.py
+++ novastub/__init__.py
@@ -5,40 +5,39 @@ import uuid
 
 app = Flask(__name__)
 hypervisors = ['21-2-66-429063', '21-4-67-429065']
-tenant_id = 1234
 
 @app.route('/')
 def api_root():
     return 'Welcome to nova admin api stub'
 
-@app.route('/%s/rax-fg-migrations' % tenant_id, methods=['POST'])
-def create_instance():
+@app.route('/<tenant_id>/rax-fg-migrations', methods=['POST'])
+def create_instance(tenant_id):
   message = {"instance_uuid": str(uuid.uuid4())}
   resp = jsonify(message)
   return resp
 
-@app.route('/%s/rax-fg-migrations/<instance_id>/activate' % tenant_id, methods=['POST'])
-def activate_instance(instance_id):
+@app.route('/<tenant_id>/rax-fg-migrations/<instance_id>/activate', methods=['POST'])
+def activate_instance(tenant_id, instance_id):
   message = {'status': 204, 'message': 'No content'}
   resp = jsonify(message)
   resp.status_code = 204
   return resp
 
-@app.route('/%s/rax-fg-migrations/<instance_id>' % tenant_id, methods=['DELETE'])
-def delete_instance(instance_id):
+@app.route('/<tenant_id>/rax-fg-migrations/<instance_id>', methods=['DELETE'])
+def delete_instance(tenant_id, instance_id):
   message = {'status': 204, 'message': 'No content'}
   resp = jsonify(message)
   resp.status_code = 204
   return resp
 
-@app.route('/%s/rax-fg-migrations/<instance_id>' % tenant_id)
-def show_instance(instance_id):
+@app.route('/<tenant_id>/rax-fg-migrations/<instance_id>')
+def show_instance(tenant_id, instance_id):
   message = {'hypervisor_hostname': random.choice(hypervisors)}
   resp = jsonify(message)
   return resp
 
-@app.route('/%s/flavors/<flavor_id>/action' % tenant_id, methods=['POST'])
-def add_flavor_access(flavor_id):
+@app.route('/<tenant_id>/flavors/<flavor_id>/action', methods=['POST'])
+def add_flavor_access(tenant_id, flavor_id):
   message = {"status": 204, "message":"No Content"}
   resp = jsonify(message)
   resp.status_code = 204
